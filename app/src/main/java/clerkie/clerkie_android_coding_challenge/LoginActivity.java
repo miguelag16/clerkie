3762fa1a8974e4fadc4cfa88df7bf7ac80aa17df
Miguel A. Garcia <miguel16@gmail.com>
2018-08-23 13:37:19 -0700

    Added Material Design in dependencies

--- app/src/main/java/clerkie/clerkie_android_coding_challenge/LoginActivity.java
+++ app/src/main/java/clerkie/clerkie_android_coding_challenge/LoginActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Build;
